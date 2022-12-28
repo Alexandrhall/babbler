e1d1f0b66e3890c3acf9d49dde449b14a0ec84ef
Alexandrhall <alexandrhall@gmail.com>
2022-12-28 21:09:15 +0100

    authContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../src/redux/store";
+import { AuthProvider } from "../src/contexts/authContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
     </Provider>
   );
 }
---
+++ src/contexts/authContext.tsx
@@ -0,0 +1,93 @@
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { signInWithEmailAndPassword, User } from "firebase/auth";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { auth } from "../firebase";
+import { updateDetails } from "../redux/auth";
+
+interface IChildren {
+  children: JSX.Element | JSX.Element[];
+}
+
+export const AuthContext = createContext<User | null>(null);
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function login(email: string, password: string) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+export function logout() {
+  return auth.signOut();
+}
+
+export const AuthProvider = ({ children }: IChildren) => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const dispatch = useAppDispatch();
+  const authRedux = useAppSelector((state) => state.auth);
+
+  const logoutFailedLogin = useCallback(() => {
+    logout();
+    dispatch(
+      updateDetails({
+        user: {
+          id: "",
+          role: "",
+          email: "",
+        },
+        msg: "Failed to login",
+      })
+    );
+  }, [dispatch]);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  //   useEffect(() => {
+  //     if (currentUser) {
+  //       const getUser = async () => {
+  //         try {
+  //           const promise = await getUserDetails(currentUser.uid);
+  //           if (promise && promise) {
+  //             dispatch(
+  //               updateDetails({
+  //                 user: {
+  //                   id: promise.id,
+  //                   role: promise.role,
+  //                   email: promise.email,
+  //                 } as IUserDetails,
+  //                 msg: "",
+  //               })
+  //             );
+  //           } else {
+  //             logoutFailedLogin();
+  //           }
+  //         } catch (err) {
+  //           logoutFailedLogin();
+  //           console.log("Promise error", err);
+  //         }
+  //       };
+  //       getUser();
+  //     }
+  //   }, [currentUser, dispatch, logoutFailedLogin]);
+
+  return (
+    <AuthContext.Provider value={currentUser}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
