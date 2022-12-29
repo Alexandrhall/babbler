ca8ae2a4278a2605d31d42ea5c21f518e8c6a770
Alexandrhall <alexandrhall@gmail.com>
2022-12-29 20:08:10 +0100

    login och logout

--- pages/index.tsx
+++ pages/index.tsx
@@ -99,6 +99,7 @@ export default function Home() {
                   msg: "",
                 })
               );
+              router.push("/login");
             }}
           >
             logout
