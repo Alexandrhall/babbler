d6470833f036f066b525a4dfb12c6e2194151e7e
Alexandrhall <alexandrhall@gmail.com>
2023-01-07 13:11:33 +0100

    cleaned index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,10 @@
 import styles from "../styles/Home.module.css";
-import { FormEvent, ReactNode, useEffect, useRef, useState } from "react";
-import {
-  collection,
-  addDoc,
-  getDocs,
-  query,
-  orderBy,
-  limit,
-  serverTimestamp,
-  where,
-  getDoc,
-} from "firebase/firestore";
-import { database } from "../src/firebase";
-import Button from "@mui/material/Button";
+import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../src/redux/hooks";
 import { useRouter } from "next/router";
 import { useAuth } from "../src/contexts/authContext";
 import ChatRoom from "../src/components/ChatRoom";
-import { List, ListItemText } from "@mui/material";
 import Navbar from "../src/components/Navbar";
-import Link from "next/link";
-import {
-  useCollection,
-  useCollectionData,
-  useCollectionDataOnce,
-} from "react-firebase-hooks/firestore";
-import roomConverter, { userConverter } from "../src/services/postConverter";
 import RoomList from "../src/components/RoomList";
 
 interface IChildren {
@@ -33,7 +12,6 @@ interface IChildren {
 }
 
 export default function Home({ children }: IChildren) {
-  const dispatch = useAppDispatch();
   const router = useRouter();
   const user = useAuth();
   const auth = useAppSelector((state) => state.auth);
