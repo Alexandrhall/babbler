ae4a7b9215539b0dfd359f3eeb3b6d6452a654be
Alexandrhall <alexandrhall@gmail.com>
2023-01-09 12:12:07 +0100

    usename

---
+++ src/services/useGetRoom.ts
@@ -0,0 +1,12 @@
+import { collection, query } from "firebase/firestore";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { database } from "../firebase";
+import roomConverter, { userConverter } from "./postConverter";
+
+export const useGetRoom = (collectionRef: string) => {
+  const ref = collection(database, collectionRef).withConverter(roomConverter);
+  const q = query(ref);
+  const [room] = useCollectionData(q);
+
+  return [room];
+};
