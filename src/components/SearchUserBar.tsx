7786aa35441a094bdaac513d4bbc73589d01c4fa
Alexandrhall <alexandrhall@gmail.com>
2023-01-10 16:02:30 +0100

    searchbar progress

--- src/components/SearchUserBar.tsx
+++ src/components/SearchUserBar.tsx
@@ -4,10 +4,15 @@ import Autocomplete from "@mui/material/Autocomplete";
 import { useGetUsers } from "../services/useGetUsers";
 import { TUser } from "../services/postConverter";
 import { useGetRoom } from "../services/useGetRoom";
+import { useAppSelector } from "../redux/hooks";
+import router from "next/router";
+import { addDoc, collection } from "firebase/firestore";
+import { database } from "../firebase";
 
 export default function SearchUserBar() {
   const [usrr] = useGetUsers();
   const [dm] = useGetRoom("directMessages");
+  const auth = useAppSelector((state) => state.auth);
   let users: TUser[] = [];
   const [usrId, setUsrId] = React.useState("");
 
@@ -15,25 +20,46 @@ export default function SearchUserBar() {
     users = [...usrr];
   }
 
-  const handleChange = (
+  const handleChange = async (
     e: React.SyntheticEvent<Element, Event>,
     value: TUser | null
   ) => {
-    if (value?.id !== undefined) {
-      console.log(value.id);
-      dm && dm;
+    if (value?.id !== undefined && value?.id !== "") {
+      let create = false;
+      //   const res =
+      dm &&
+        dm.filter((room) => {
+          if (
+            room.users.includes(value.id) &&
+            room.users.includes(auth.user.id)
+          ) {
+            router.push(`/directmessage/${room.id}`);
+          } else {
+            console.log("run");
+            create = true;
+          }
+        });
+      if (create) {
+        try {
+          await addDoc(collection(database, "directMessages"), {
+            messages: [],
+            users: [auth.user.id, value.id],
+          });
+          create = false;
+        } catch (err) {
+          console.error(err);
+        }
+      }
     }
   };
-
-  React.useEffect(() => {
-    console.log(usrId);
-  }, [usrId]);
 
   return (
     <Autocomplete
       disablePortal
       id="combo-box-demo"
-      options={users.map((usr) => usr)}
+      options={users.filter((usr) => {
+        if (usr.id !== auth.user.id) return usr;
+      })}
       getOptionLabel={(usr) => usr.username}
       isOptionEqualToValue={(option, value) => option.id === value.id}
       onChange={handleChange}
